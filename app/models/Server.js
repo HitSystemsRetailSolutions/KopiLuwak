be6ece7188a246d161ce3bd802e523086472b035
Alejandro <alexvidalcasado@gmail.com>
2023-11-22 16:19:39 +0100

    first commit

---
+++ app/models/Server.js
@@ -0,0 +1,9 @@
+export class Server {
+  constructor(id, name, ip, port, status) {
+    this.id = id;
+    this.name = name;
+    this.ip = ip;
+    this.port = port;
+    this.status = status;
+  }
+}
