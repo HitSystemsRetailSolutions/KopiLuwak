6202a3898d69fbd210b9236896fa0b2387446cb1
Alejandro <alexvidalcasado@gmail.com>
2023-11-27 16:39:18 +0100

    top 3

--- app/routes/db.js
+++ app/routes/db.js
@@ -4,9 +4,12 @@ import { recHit } from "../db/mssql.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const results = await recHit("fac_tena", "select * from dependentes");
+  const results = await recHit("fac_tena", "select TOP 3 * from dependentes");
 
   res.json(results.recordset);
 });
+
+// select d.codi,d.nom,telefon,[adreça],e.valor from dependentes d join (select * from dependentesextes where nom = 'hBase') e on e.id=d.codi
+// select tmst,iif(accio = 1 , 'inici Torn' ,'Fi Torn') ,d.nom,c.nom from cdpDadesFichador f join dependentes d on d.codi=f.usuari join clients c on c.codi=f.lloc
 
 export default router;
