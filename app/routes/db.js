77ed4e31c6a8a7517eec21dee330dd9a386a2ce0
Alejandro <alexvidalcasado@gmail.com>
2023-11-27 17:18:03 +0100

    me obligan

--- app/routes/db.js
+++ app/routes/db.js
@@ -19,7 +19,24 @@ router.get("/", async (req, res) => {
   res.json(results.recordset);
 });
 
-// select d.codi,d.nom,telefon,[adreça],e.valor from dependentes d join (select * from dependentesextes where nom = 'hBase') e on e.id=d.codi
-// select tmst,iif(accio = 1 , 'inici Torn' ,'Fi Torn') ,d.nom,c.nom from cdpDadesFichador f join dependentes d on d.codi=f.usuari join clients c on c.codi=f.lloc
+router.get("/getTurnosDependienta", async (req, res) => {
+  const { dependienta, fecha } = req.body;
+
+  const results = await recHit(
+    "fac_tena",
+    `select tmst as fecha,iif(accio = 1 , 'inici Torn' ,'Fi Torn') as accio ,d.nom as nombreDependienta ,c.nom as nombreTienda from cdpDadesFichador f join dependentes d on d.codi=f.usuari join clients c on c.codi=f.lloc where d.nom like '%${dependienta}%' and tmst > ${fecha} order by fecha`
+  );
+
+  res.json(results.recordset);
+});
+
+router.get("/dependientas", async (req, res) => {
+  const results = await recHit(
+    "fac_tena",
+    "select d.codi,d.nom,telefon,[adreça],e.valor from dependentes d join (select * from dependentesextes where nom = 'hBase') e on e.id=d.codi"
+  );
+
+  res.json(results.recordset);
+});
 
 export default router;
