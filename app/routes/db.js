d58faf7e26c992121faf86dbea474b4f05e9a08b
Alejandro <alexvidalcasado@gmail.com>
2023-11-28 17:22:26 +0100

    a

--- app/routes/db.js
+++ app/routes/db.js
@@ -34,7 +34,7 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/sql", async (req, res) => {
-  const { sql } = req.body;
+  const { sql } = req.params;
 
   try {
     const results = await recHit("fac_tena", sql);
